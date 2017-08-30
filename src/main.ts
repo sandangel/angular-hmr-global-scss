d4b78c152d5424d2e9e96c793c0827d945105908
San Nguyen <sand@Sans-iMac.local>
2017-08-30 21:52:19 +0900

    hmr and global scss loader

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from "@angular/core"
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic"
 
-import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
+import { AppModule } from "./app/app.module"
+import { environment } from "./environments/environment"
+
+import { hmrBootstrap } from "./hmr"
 
 if (environment.production) {
-  enableProdMode();
+  enableProdMode()
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+const bootstrap = () => platformBrowserDynamic().bootstrapModule(AppModule)
+
+if (module["hot"]) {
+  hmrBootstrap(module, bootstrap)
+} else {
+  console.error("HMR is not enabled for webpack-dev-server!")
+  console.log("Are you using the --hmr flag for ng serve?")
+  bootstrap()
+}
